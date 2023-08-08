fd73d1e1582395c091af9724c33a3abd779d1450
dennisnderitu254 <dknderitu@gmail.com>
2023-08-08 18:28:19 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,7 +40,7 @@ class BasicAuth(Auth):
         # return authorization_header[6:]
 
     def decode_base64_authorization_header(
-        self, base64_authorization_header: str) -> str:
+            self, base64_authorization_header: str) -> str:
         """
         returns the decoded value of a Base64
         string base64_authorization_header
