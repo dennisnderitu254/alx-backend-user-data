47367033fe521ee912b6c9a8ff2d0e029fa5d725
dennisnderitu254 <dknderitu@gmail.com>
2023-08-10 09:13:49 +0300

    adding a new endpoint: GET /users/me to retrieve the authenticated User object

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -27,18 +27,14 @@ def before_request():
     """
     handler before_request
     """
-    authorized_list = ['/api/v1/status/',
-                       '/api/v1/unauthorized/', '/api/v1/forbidden/',
-                       '/api/v1/auth_session/login/']
+    authorized_list = ['/api/v1/status',
+                       '/api/v1/unauthorized/', '/api/v1/forbidden']
 
     if auth and auth.require_auth(request.path, authorized_list):
         if not auth.authorization_header(request):
             abort(401)
-        if (auth.authorization_header(request) and
-            not auth.session_cookie(request)):
-            abort(401)
         request.current_user = auth.current_user(request)
-        if not auth.current_user(request):
+        if not request.current_user:
             abort(403)
 
 
