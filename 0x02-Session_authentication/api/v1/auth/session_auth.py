22c96abc2f2381d5d440a392c736e7185e260c04
dennisnderitu254 <dknderitu@gmail.com>
2023-08-10 17:22:50 +0300

    Session Authentication - Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ from api.v1.auth.auth import Auth
 from typing import TypeVar
 from uuid import uuid4
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
@@ -17,19 +18,27 @@ class SessionAuth(Auth):
         """
         Create a Session ID for a user_id
         """
-        if not user_id or type(user_id) != str:
-            return
-        session_id = str(uuid4())
-        SessionAuth.user_id_by_session_id[user_id] = session_id
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
+        # if not user_id or type(user_id) != str:
+        #     return
+        # session_id = str(uuid4())
+        # SessionAuth.user_id_by_session_id[user_id] = session_id
+        # return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
         returns a User ID based on a Session ID
         """
-        if not session_id or type(session_id) != str:
-            return
-        return SessionAuth.user_id_by_session_id.get(session_id, None)
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
+        # if not session_id or type(session_id) != str:
+        #     return
+        # return SessionAuth.user_id_by_session_id.get(session_id, None)
 
     # def current_user(self, request=None):
     #     """
