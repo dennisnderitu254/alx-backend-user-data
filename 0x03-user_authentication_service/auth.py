4b2915427f76628561b197dc4556f9bcda2e1632
dennisnderitu254 <dknderitu@gmail.com>
2023-08-16 16:23:55 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,6 +32,16 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        valid login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
 # # Test the _hash_password method
 # if __name__ == "__main__":
 #     password = "Hello Holberton"
